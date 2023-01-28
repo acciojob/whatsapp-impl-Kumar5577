98184fe2c49497b2cb1e078c276c4f8d3764e7a6
RAMYA5577 <mutyamramya0101@gmail.com>
2023-01-28 20:43:44 +0530

    changes made

--- src/main/java/com/driver/Message.java
+++ src/main/java/com/driver/Message.java
@@ -7,7 +7,15 @@ public class Message {
     private String content;
     private Date timestamp;
 
-    public Message() {
+    public Message(int id) {
+        this.id = id;
+        this.timestamp = new Date();
+    }
+
+    public Message(int id, String content) {
+        this.id = id;
+        this.content = content;
+        this.timestamp = new Date();
     }
 
     public Message(int id, String content, Date timestamp) {
